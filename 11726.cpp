1c71f7ebe8919ce498536d989c6a69972dad1d97
NOAsArKknights <ybzxc9249@gmail.com>
2026-03-18 21:47:37 +0900

    Create 11726.cpp
    <2xn 타일링>
    Algorithm : Dp

---
+++ 11726.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int arr[1003];
+
+int main()
+{
+    int i,n;
+    
+    scanf("%d",&n);
+
+    arr[1]=1;
+    arr[2]=2;
+    
+    for(i=3;i<=n;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+        arr[i]%=10007;
+    }
+    
+    printf("%d\n",arr[n]);
+    
+    return 0;
+}
